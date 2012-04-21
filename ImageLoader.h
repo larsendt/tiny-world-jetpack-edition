399bc187099ce50444c47ce5ef59eceeca74b8f2
Ken Hoff <kenhoff@Europa.local>
2012-04-21 01:12:55 -0600

    more apple shit

--- ImageLoader.h
+++ ImageLoader.h
@@ -1,5 +1,9 @@
 #include <SFML/Graphics.hpp>
+#ifdef __APPLE__
+#include <OpenGL/glu.h>
+#else
 #include <GL/glu.h>
+#endif
 #include <stdio.h>
 GLuint loadImage(char* file);
 void bindImage(GLuint img);
